262f5b99aad6bc3a6be42282fe1c3742eb3de9eb
Akshay <akshaybeniwal12@gmail.com>
2019-10-30 20:41:42 +0530

    Removing console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side java script')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.querySelector('#m1')
